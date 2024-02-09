78d41acdb852c968fbd2522e11af41045dd302dc
raishudesu <bacaltosbaryshnikov@gmail.com>
2024-02-09 15:39:37 +0800

    add templates

--- searcher/views.py
+++ searcher/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Thesis
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("hello")
+    theses = Thesis.objects.all()
+    return render(request, "base.html", {"theses": theses})
+
+
+# def thesis_list(request):
+# return render(request, "searcher/theses.html", {"theses": theses})
