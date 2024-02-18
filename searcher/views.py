c0f16b0d6e1b17fee354d5f99e1d2f3c946ceb81
raishudesu <bacaltosbaryshnikov@gmail.com>
2024-02-18 20:49:08 +0800

    update project structure

--- searcher/views.py
+++ searcher/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Thesis
 
@@ -9,11 +9,12 @@ def index(request):
     theses = Thesis.objects.all()
 
     context = {"theses": theses}
-    return render(request, "base.html", context)
+    return render(request, "searcher/thesis/list.html", context)
 
 
 def thesis_detail(request, id):
-    thesis = Thesis.objects.get(pk=id)
+    # thesis = Thesis.objects.get(pk=id)
+    thesis = get_object_or_404(Thesis, pk=id)
     authors = thesis.authors.all()
     panelists = thesis.panelists.all()
     keywords = thesis.keywords.all()
@@ -25,4 +26,4 @@ def thesis_detail(request, id):
         "keywords": keywords,
     }
 
-    return render(request, "detail.html", context)
+    return render(request, "searcher/thesis/detail.html", context)
